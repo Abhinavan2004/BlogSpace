d96018379c3a76df5b2f2d0c0b6235eae73577a0
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-09 01:13:27 +0530

    Added backend files

---
+++ BlogSpace-Backend/src/main/java/com/example/demo/BlogSpaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSpaceBackendApplication.class, args);
+	}
+
+}
