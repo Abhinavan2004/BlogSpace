e924f8cfe9c1b88d1f2463d0a82abf7fef14e080
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-09 12:18:53 +0530

    Added Tag Entity

--- BlogSpace-Backend/src/main/java/com/example/demo/entity/Entity_Tags.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/entity/Entity_Tags.java
@@ -2,26 +2,23 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 import java.util.UUID;
 
 @Entity
-@AllArgsConstructor
+@Builder
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-@Table(name="Category_Table")
-@Builder
-public class Entity_Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID id;
-
+@Table(name="Tags_Table")
+public class Entity_Tags {
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        Entity_Category that = (Entity_Category) o;
+        Entity_Tags that = (Entity_Tags) o;
         return Objects.equals(id, that.id) && Objects.equals(name, that.name);
     }
 
@@ -30,6 +27,10 @@ public class Entity_Category {
         return Objects.hash(id, name);
     }
 
-    @Column(unique = true , nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
     private String name;
 }
