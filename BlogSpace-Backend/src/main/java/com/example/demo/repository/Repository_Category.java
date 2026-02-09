366061744597241b9c3eb20231c119662ee8e4ec
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-09 13:01:48 +0530

    Added Tag Repo

--- BlogSpace-Backend/src/main/java/com/example/demo/repository/Repository_Category.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/repository/Repository_Category.java
@@ -2,8 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.entity.Entity_Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface Repository_Category extends JpaRepository<Entity_Category, UUID> {
 }
