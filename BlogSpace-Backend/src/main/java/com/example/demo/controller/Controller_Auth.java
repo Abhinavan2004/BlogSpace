b0095b691f1703c430c8650a4a3b6e69deb456d0
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-13 15:16:43 +0530

    Added some files

--- BlogSpace-Backend/src/main/java/com/example/demo/controller/Controller_Auth.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/controller/Controller_Auth.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.dtos.AuthResponse;
+import com.example.demo.service.Service_Auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class Controller_Auth {
 
+    private final Service_Auth service_auth;
+
     @PostMapping
-    public ResponseEntity<>
+    public ResponseEntity<AuthResponse> login(@RequestBody UserDetails userDetails){
+        UserDetails user = service_auth.authenticate(
+                userDetails.getUsername(),
+                userDetails.getPassword()
+        );
+        String tokenValue = service_auth.generateToken(user);
+
+        AuthResponse resp = AuthResponse.builder()
+                .token(tokenValue)
+                .expiresIn(86400)
+                .build();
+        return ResponseEntity.ok(resp);
+    }
 }
---
+++ BlogSpace-Backend/src/main/java/com/example/demo/service/Service_Auth.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface Service_Auth {
+    UserDetails authenticate(String email, String password);
+    String generateToken(UserDetails userDetails);
+}
