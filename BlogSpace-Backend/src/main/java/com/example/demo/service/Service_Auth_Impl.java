e6476b79fd3de4cf9fbd0471fc7a1e3aeba959fc
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-21 11:38:39 +0530

    Adding Jwt secret

--- BlogSpace-Backend/src/main/java/com/example/demo/service/Service_Auth_Impl.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/service/Service_Auth_Impl.java
@@ -1,17 +1,34 @@
 package com.example.demo.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class Service_Auth_Impl implements Service_Auth{
+
+    private AuthenticationManager authenticationManager;
+    private UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret}")
+    private String SecretKey ;
+
     @Override
     public UserDetails authenticate(String email, String password) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+        return  userDetailsService.loadUserByUsername(email);
     }
 
     @Override
     public String generateToken(UserDetails userDetails) {
-        return "";
+
     }
 }
