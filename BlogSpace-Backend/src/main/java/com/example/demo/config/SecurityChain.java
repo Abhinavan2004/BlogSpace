a4f3b01949b25e09c32b4f5ea48dbf21b575a836
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-21 12:28:43 +0530

    Added jwtauthenticationfilter in securitychain

--- BlogSpace-Backend/src/main/java/com/example/demo/config/SecurityChain.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/config/SecurityChain.java
@@ -1,24 +1,33 @@
 package com.example.demo.config;
 
+import com.example.demo.security.JwtAuthenticationFilter;
+import com.example.demo.service.Service_Auth_Impl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityChain {
 
+    @Bean
+public JwtAuthenticationFilter  jwtAuthenticationFilter(Service_Auth_Impl service_Auth_Impl){
+    return new JwtAuthenticationFilter(service_Auth_Impl) ;
+}
 
     @Bean
-    public SecurityFilterChain securityfilterchain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityfilterchain(HttpSecurity http , JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth", "/api/v1/auth").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts", "/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories", "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags", "/api/v1/tags/**").permitAll()
@@ -27,7 +36,7 @@ public class SecurityChain {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                ).addFilterBefore(jwtAuthenticationFilter , UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
