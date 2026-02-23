ecdb84fe7f63e245cbafcc0f3b64f2cc2ec33ef5
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-23 18:21:50 +0530

    Added author dto and post dto

--- BlogSpace-Backend/src/main/java/com/example/demo/domain/dtos/Dto_Author.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/domain/dtos/Dto_Author.java
@@ -3,16 +3,16 @@ package com.example.demo.domain.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
-@Builder
 @AllArgsConstructor
-@RequiredArgsConstructor
-public class Tags_Response {
-    private UUID id;
+@NoArgsConstructor
+@Builder
+public class Dto_Author {
+    private UUID id ;
     private String name ;
-    private long postCount ;
+
 }
--- BlogSpace-Backend/src/main/java/com/example/demo/domain/dtos/Dto_Posts.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/domain/dtos/Dto_Posts.java
@@ -1,4 +1,26 @@
 package com.example.demo.domain.dtos;
 
+import com.example.demo.domain.Enum_Post;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Dto_Posts {
+    private UUID id;
+    private String title;
+    private String content;
+    private Dto_Author author ;
+    private Dto_Category category;
+    private Set<Dto_Tag> tags;
+    private Integer readingTime ;
+    private LocalDateTime createdAt ;
+    private LocalDateTime updatedAt ;
+    private Enum_Post postStatus ;
+
 }
