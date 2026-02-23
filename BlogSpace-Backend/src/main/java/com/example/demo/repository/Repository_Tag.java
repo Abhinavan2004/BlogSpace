c6d4e822f5968ecb28088877c7803b24ca419b8f
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-23 12:25:19 +0530

    Added repo , service  , controller of tag

--- BlogSpace-Backend/src/main/java/com/example/demo/repository/Repository_Tag.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/repository/Repository_Tag.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface Repository_Tag extends JpaRepository<Entity_Tags, UUID> {
+    List<Entity_Tags> findAllWithPostCount();
 }
