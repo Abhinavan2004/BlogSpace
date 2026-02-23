496f8cf5d9b7b7aa37ec932a5faab192d118a503
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-23 18:34:58 +0530

    Added A new find category id in service category

--- BlogSpace-Backend/src/main/java/com/example/demo/controller/Controller_Post.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/controller/Controller_Post.java
@@ -3,13 +3,21 @@ package com.example.demo.controller;
 import com.example.demo.domain.dtos.Dto_Posts;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/posts")
 public class Controller_Post {
 
-    public ResponseEntity<List<Dto_Posts>> getAllPosts(){
+    public ResponseEntity<List<Dto_Posts>> getAllPosts(
+            @RequestParam(required = false) UUID categoryId,
+            @RequestParam(required = false) UUID tagId) {
+
 
     }
 }
+
