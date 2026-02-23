d88c1c227334ee9eeee69a382b5f6dcc0f25b9a2
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-23 12:52:46 +0530

    edited Dto_Tags -> Tags_Response

--- BlogSpace-Backend/src/main/java/com/example/demo/repository/Repository_Tag.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/repository/Repository_Tag.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.entity.Entity_Tags;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,6 @@ import java.util.UUID;
 
 @Repository
 public interface Repository_Tag extends JpaRepository<Entity_Tags, UUID> {
+    @Query("Select t from Entity_Tags t Left join fetch t.posts")
     List<Entity_Tags> findAllWithPostCount();
 }
