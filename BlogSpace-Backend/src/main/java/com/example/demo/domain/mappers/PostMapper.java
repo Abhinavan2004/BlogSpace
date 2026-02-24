57afbf4dece619d37f021716479b908afadd0f63
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-25 02:47:50 +0530

    Updated Controller , Service , Dto for Posts

---
+++ BlogSpace-Backend/src/main/java/com/example/demo/domain/mappers/PostMapper.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain.mappers;
+
+import com.example.demo.domain.dtos.Dto_Posts;
+import com.example.demo.domain.entity.Entity_Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring" , unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PostMapper {
+    @Mapping(target = "author" , source = "author")
+    @Mapping(target="category" , source = "category")
+    @Mapping(target="tags" , source="tags")
+    Dto_Posts toDto(Entity_Post post);
+}
