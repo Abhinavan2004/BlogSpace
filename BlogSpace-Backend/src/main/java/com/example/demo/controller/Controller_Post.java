00750e1ca74c4c611ecc7631f6bfcbd0f46fc1de
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-25 02:47:50 +0530

    Updated Controller , Service , Dto for Posts

--- BlogSpace-Backend/src/main/java/com/example/demo/controller/Controller_Post.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/controller/Controller_Post.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.dtos.Dto_Posts;
+import com.example.demo.domain.entity.Entity_Post;
+import com.example.demo.domain.mappers.PostMapper;
+import com.example.demo.service.Service_Posts;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +16,19 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
 public class Controller_Post {
 
+    private final Service_Posts postService;
+    private final PostMapper postMapper;
+
+    @GetMapping
     public ResponseEntity<List<Dto_Posts>> getAllPosts(
             @RequestParam(required = false) UUID categoryId,
             @RequestParam(required = false) UUID tagId) {
-
-
+        List<Entity_Post> posts = postService.getAllPosts(categoryId, tagId);
+        List<Dto_Posts> postDtos = posts.stream().map(postMapper::toDto).toList();
+        return ResponseEntity.ok(postDtos);
     }
 }
 
