e838c21acded09ef34e7ef76491fedf9471111ea
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-25 00:02:12 +0530

    Added Service of posts and repository

--- BlogSpace-Backend/src/main/java/com/example/demo/repository/Repository_Post.java
+++ BlogSpace-Backend/src/main/java/com/example/demo/repository/Repository_Post.java
@@ -7,10 +7,14 @@ import com.example.demo.domain.entity.Entity_Tags;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface Repository_Post extends JpaRepository<Entity_Post, UUID> {
     List<Entity_Post> findAllByStatusAndCategoryAndTags(Enum_Post status, Entity_Category category, Entity_Tags tags);
+    List<Entity_Post> findAllByStatusAndCategory(Enum_Post status, Entity_Category category);
+    List<Entity_Post> findAllByStatusAndTagsContaining(Enum_Post status , Entity_Tags tags);
+    List<Entity_Post> findAllByStatus(Enum_Post status);
 }
